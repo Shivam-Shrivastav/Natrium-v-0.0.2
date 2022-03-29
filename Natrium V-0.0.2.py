a13d0d5c8e7258c29b64ce6c361ef9334af6419f
yogesh <yogesh.dubey.0804@gmail.com>
2022-03-30 00:49:07 +0530

    sample lambda code

--- Natrium V-0.0.2.py
+++ Natrium V-0.0.2.py
@@ -1,90 +1,42 @@
-'''
-NATRIUM V-0.0.2
-
-
-CREATE A FUNCTION IN PYTHON IN ORDER TO FILTER BEST MATCHING USER FROM AN ARRAY OF USER 
-
-FILTERATION IS BASED ON THE NUMBER OF MATCHES BETWEEN THE USER AND THE USER IN THE ARRAY
+import json
+import boto3    
 
 
 
-FILTERATION IS BASED PRIMARILY ON THE TYPE PROPERTY UNDER FILTERS 
-
-IF A USER IS WITH STRICT filter["type"] THEN THE USER WILL BE FILTERED WITH ONLY THE USERS MATHCHING THE FILTER TYPE ONLY /-
-ELIF A USER IS WITH filter["type"] = "ANY" THEN THE USER WILL BE FILTERED WITH THE DEFINED WAY /-
-
-
-BELOW LISTED ARE THE PRIORITIES 
-
-
-1. user["filter"]["location"]
-2. user["age"]
-3. user["gender"]
-5. user ['languages']
-4. user['profession']
-5. user['AccountType']
-6. user['Interest1'], user['Interest2'],user['Interest3']
-
-
-
-
-
-1. CREATE A FUNCTION THAT TAKES IN AN ARRAY OF USERS AND A USER
-2. CREATE A FOR LOOP THAT ITERATES THROUGH THE ARRAY OF USERS
-3. CREATE A FOR LOOP THAT ITERATES THROUGH THE USER'S PREFERENCES
-
-
-
-'''
-
-
-
-
-user = {
- "id": "0a45c971-aa4d-4521-9d4a-a2bb24ba15cb",
- "__typename": "User",
- "following": 80,
- "EmailId": "Pragya@ayesavi.com",
- "_lastChangedAt": 1648577463231,
- "followers": 90,
- "createdAt": "2022-03-29T18:11:03.197Z",
- "UserName": "pragya_meshram@aye",
- "Profession": "Engineer",
- "Gender": "FEMALE",
- "_version": 1,
- "PhoneNumber": "+89632987429",
- "updatedAt": "2022-03-29T18:11:03.197Z",
- "Stories": [],
- "userConfig": {
-  "Interest1": " Food",
-  "location": {
-      "country":"canada",
-      "city":"vancouver"
-  },
-  "Interest3": "coding",
-  "Interest2": "love",
-  "ShortDescription": "Im a aeronautical  engineer working with national aeronautics"
- },
- "AccountType": "PUBLIC",
- "Name": "pragya meshram",
- "Age": "42",
- "languages":["English","Czech","Spanish","French","Hindi"],
- "filters":{
-     "type":"strict",     # 1st priority 
-      "regional_Filter": True,  # 2nd priority
-      "category":{
-          "Profession":"Engineer",
-          "Gender":"Male",
-          "location":"India",
-          "languages":["french"]
-      }
- }
+def lambda_handler(event, context):
+   
+    URl =  "https://XX.XX.XX.XX.amazonaws.com/production"
+    dynamo = boto3.client("dynamodb")
+    client_id = str(event["requestContext"].get("connectionId"))
+    usersArray = json.loads(event["body"]).get("connectedUsersArray")
+    print(usersArray)
+    client = boto3.client("apigatewaymanagementapi",endpoint_url = URl)
+    value  = 0
+    message = {
+        "type":"byAnotherUser",
+        "partnerConnectionId":client_id,
+    }
+    response = dynamo.scan(TableName="Websockets_ConnectionIDS")
+    print(response)
+    userids = response["Items"]
+    settosend = {}
+    if userids == []:
+         dynamo.put_item(TableName="Websockets_ConnectionIDS",Item= {"connectionId":{"S":client_id}} )
+    
+    else:
+        for USERconnectionIDS in userids:
+            value = USERconnectionIDS.get("connectionId").get("S")
+            if value != client_id and value not in  usersArray:
+                dynamo.delete_item(TableName = "Websockets_ConnectionIDS",Key = {"connectionId":{"S":value}})
+                settosend = {
+                       "partnerConnectionId":value,
+                        "type":"ready",
+                        "yourID":client_id,
+                }
+                message["yourID"] = value;
+                respond = client.post_to_connection(ConnectionId = value,Data=json.dumps(message))
+    return {
+    'statusCode': 200,
+    'headers': {'Content-Type': 'application/json'},
+    'body': json.dumps(settosend)
 }
-
-
-
-# fromUsers would hold array of users having userconfigurations just like user above given :- 
-def Recommend_User(forUser = {},fromUsers = []):
-    # your function here
-    print()
-
